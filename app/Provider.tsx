b98a72616d0b935386e18d898f926b450bd28fc8
abdullaO7 <abdallahalhsam09@gmail.com>
2025-01-03 15:03:07 +0300

    user Mention featuers

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -3,16 +3,34 @@ import React from 'react'
 
 import { LiveblocksProvider, ClientSideSuspense } from '@liveblocks/react/suspense';
 import Loader from '@/components/Loader';
-import { getClerkUsers } from '@/lib/actions/users.actions';
+import { getClerkUsers, getMentionSuggestions } from '@/lib/actions/users.actions';
+import { useUser } from '@clerk/nextjs';
 
 
 
 const Provider = ({children} : {children : React.ReactNode}) => {
+
+    const {user : clerkUser} = useUser();
+
     return (
-        <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"} resolveUsers={async({userIds})=> {
+        <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"} 
+        resolveUsers={async({userIds})=> {
             const users = await getClerkUsers({userIds})
             return users;
-        }}>
+        }} 
+            resolveMentionSuggestions={async ({text , roomId}) => {
+                const userEmail = clerkUser?.emailAddresses[0].emailAddress;
+                if (!userEmail) return [];
+
+                const roomUsers = await getMentionSuggestions({
+                    roomId: roomId,
+                    currentUser: userEmail,
+                    text: text
+                });
+
+                return roomUsers;
+            }}
+        >
             <ClientSideSuspense fallback={<Loader/>}>
                 {children}
             </ClientSideSuspense>
--- lib/actions/users.actions.ts
+++ lib/actions/users.actions.ts
@@ -1,6 +1,7 @@
 'use server'
 import { clerkClient } from "@clerk/nextjs/server"
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
     try {
@@ -25,6 +26,30 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
         return parseStringify(sortedUsers);
     } catch (error) {
         console.log(`Error fetching users: ${error}`);
+        return null;
+    }
+}
+
+
+export const getMentionSuggestions = async ({text , roomId , currentUser} : {text : string , roomId : string , currentUser : string}) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        if(!room) return [];
+
+        const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+        if(text.length){
+            const lowerCaseText = text.toLowerCase();
+
+            const filteredUser = users.filter((email : string ) => email.toLowerCase().includes(lowerCaseText));
+
+            return parseStringify(filteredUser);
+        }
+        return parseStringify(users);
+
+    } catch (error) {
+        console.log(`Error fetching mention suggestions: ${error}`);
         return null;
     }
 }
